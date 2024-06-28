8d18d6e656c89f341938330891c491975b1de128
Samuel Araujo <samuel.araujo.carvalho@gmail.com>
2024-06-28 01:09:48 -0300

    Selec por Monitor - conexão Atlas - clear console

--- public/scripts.js
+++ public/scripts.js
@@ -1,53 +1,91 @@
-document.getElementById('dateForm').addEventListener('submit', fetchData);
+document.addEventListener('DOMContentLoaded', function() {
+    const chartContainer = document.querySelector('#chart');
+    let chart; // Variável para armazenar a instância do gráfico
 
-async function fetchData(event) {
-    event.preventDefault();
-    const startDate = document.getElementById('startDate').value;
-    const endDate = document.getElementById('endDate').value;
-    const query = `startDate=${startDate}&endDate=${endDate}`;
+    // Inicializar o gráfico vazio ao carregar a página
+    initializeEmptyChart();
 
-    try {
-        const response = await fetch(`/dados?${query}`);
-        
-        // Verifique se o conteúdo é JSON antes de tentar analisá-lo
-        const contentType = response.headers.get('content-type');
-        if (!contentType || !contentType.includes('application/json')) {
-            throw new Error('A resposta não é JSON');
+    // Adicionar evento de submit ao formulário
+    document.getElementById('dateForm').addEventListener('submit', fetchData);
+
+    async function fetchData(event) {
+        event.preventDefault();
+        const startDate = document.getElementById('startDate').value;
+        const endDate = document.getElementById('endDate').value;
+        const moqaID = document.getElementById('monitor-select').value;
+        const query = `startDate=${startDate}&endDate=${endDate}&moqaID=${moqaID}`;
+
+        // Limpar dados anteriores e gráfico
+        clearPreviousData();
+
+        try {
+            const response = await fetch(`/dados?${query}`);
+            
+            // Verificar se o conteúdo é JSON antes de tentar analisá-lo
+            const contentType = response.headers.get('content-type');
+            if (!contentType || !contentType.includes('application/json')) {
+                throw new Error('A resposta não é JSON');
+            }
+
+            const data = await response.json();
+            console.log('Dados recebidos:', data.documents); // Acesse os documentos dentro do objeto recebido
+            renderChart(data.documents); // Passe apenas os documentos para o renderChart
+        } catch (error) {
+            console.error('Erro ao buscar dados:', error);
         }
-
-        //Verificando Retorno de dados (console.log)
-        const data = await response.json();
-        console.log('Dados recebidos:', data);
-        renderChart(data);
-    } catch (error) {
-        console.error('Erro ao buscar dados:', error);
     }
-}
 
-function renderChart(data) {
-    const options = {
-        chart: {
-            type: 'line',
-            height: 350
-        },
-        series: [{
-            name: 'PM2.5',
-            data: data.map(d => d.pm25)
-        }, {
-            name: 'PM10',
-            data: data.map(d => d.pm10)
-        }, {
-            name: 'Humidade',
-            data: data.map(d => d.hum)
-        }, {
-            name: 'Temperatura Externa',
-            data: data.map(d => d.extTemp)
-        }],
-        xaxis: {
-            categories: data.map(d => new Date(d.Timestamp * 1000).toLocaleString())
+    function initializeEmptyChart() {
+        const options = {
+            chart: {
+                type: 'line',
+                height: 350
+            },
+            series: [],
+            xaxis: {
+                categories: []
+            }
+        };
+
+        chart = new ApexCharts(chartContainer, options);
+        chart.render();
+    }
+
+    function clearPreviousData() {
+        // Limpar console
+        console.clear();
+
+        // Limpar gráfico existente
+        if (chart) {
+            chart.destroy(); // Destroi a instância do gráfico existente
         }
-    };
+    }
 
-    const chart = new ApexCharts(document.querySelector('#chart'), options);
-    chart.render();
-}
+    function renderChart(data) {
+        const options = {
+            chart: {
+                type: 'line',
+                height: 350
+            },
+            series: [{
+                name: 'PM2.5',
+                data: data.map(d => d.pm25)
+            }, {
+                name: 'PM10',
+                data: data.map(d => d.pm10)
+            }, {
+                name: 'Humidade',
+                data: data.map(d => d.hum)
+            }, {
+                name: 'Temperatura Externa',
+                data: data.map(d => d.extTemp)
+            }],
+            xaxis: {
+                categories: data.map(d => new Date(d.Timestamp * 1000).toLocaleString())
+            }
+        };
+
+        chart = new ApexCharts(chartContainer, options);
+        chart.render();
+    }
+});
--- src/routes/dados.js
+++ src/routes/dados.js
@@ -2,32 +2,30 @@ const express = require('express');
 const router = express.Router();
 const DadoQualidadeAr = require('../models/dadoQualidadeAr');
 
-// Endpoint para buscar dados com base em data
+// Endpoint existente para buscar dados com base em data
 router.get('/', async (req, res) => {
     try {
-        const { startDate, endDate } = req.query;
+        const { startDate, endDate, moqaID } = req.query;
         const query = {};
 
         if (startDate && endDate) {
             query.Timestamp = {
-                $gte: Math.floor(new Date(startDate).getTime() / 1000),
-                $lte: Math.floor(new Date(endDate).getTime() / 1000)
+                $gte: new Date(startDate).getTime() / 1000,
+                $lte: new Date(endDate).getTime() / 1000
             };
         }
 
+        if (moqaID) {
+            query.moqaID = moqaID;
+        }
+    
+        //Count é um contador que acrescentei para ver o numeros de Json retornando no Postman, para futuros teste.
         const dados = await DadoQualidadeAr.find(query);
-        res.json(dados);
+        const count = dados.length;
+
+        res.json({ count: count, documents: dados });
     } catch (error) {
         res.status(500).send('Erro ao buscar dados: ' + error.message);
-    }
-});
-
-router.get('/moqaIDs', async (req, res) => {
-    try {
-        const moqaIDs = await DadoQualidadeAr.distinct('moqaID');
-        res.json(moqaIDs);
-    } catch (error) {
-        res.status(500).send('Erro ao buscar moqaIDs: ' + error.message);
     }
 });
 
