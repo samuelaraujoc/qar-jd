3468a397595af4012e9425867bf3c4e4a6a773fd
Levy pinheiro <levypinheiro2022@gmail.com>
2024-07-11 13:34:03 -0300

    atualização do mapa

--- public/scripts/apex01.js
+++ public/scripts/apex01.js
@@ -85,6 +85,48 @@ document.addEventListener('DOMContentLoaded', function () {
                 type: 'bar', // Mudança para gráfico de barras
                 height: 350
             },
+            /*colors: [
+                function({ value, seriesIndex, w}) {
+                  if (0 <= value && value<=25) {
+                    //GREEN
+                    return '#009900'
+                    
+                  }else if(25<value && value<=50) {
+                    //YELLOW
+                    return '#CCCC00'
+                    
+                  }else if(50<value && value<=75){
+                    //ORANGE
+                    return '#F8000'
+                    
+                  }else if(75<value && value<=125){
+                    //RED
+                    return '#CC0000'
+                    
+                  }else{
+                    //PURPLE
+                    return '#6600CC'
+                  }
+                },
+                function({ value, seriesIndex, w }) {
+                    if (0 <= value && value<=50) {
+                        return '#009900'
+                      //GREEN
+                    } else if(50<value && value<=100) {
+                        return '#CCCC00'
+                      //YELLOW
+                    }else if(100<value && value<=150){
+                        return '#F8000'
+                      //ORANGE
+                    }else if(150<value && value<=250){
+                        return '#CC0000'
+                      //RED
+                    }else{
+                        return '#6600CC'
+                      //PURPLE
+                    }
+                  }
+            ],*/
             series: [{
                 name: 'PM2.5',
                 data: data.map(d => d.avgPM25)
