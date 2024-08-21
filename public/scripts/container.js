e2dd315d0c1b398634c68dc7ad5b6c09cd0f3f47
Samuel Araujo <samuel.araujo.carvalho@gmail.com>
2024-08-20 23:43:07 -0300

    add new icons, condições New Conatainer

--- public/scripts/container.js
+++ public/scripts/container.js
@@ -17,36 +17,48 @@ document.addEventListener('DOMContentLoaded', function () {
                 pm25Display.textContent = pm25Value;
             
                 const pmIcon = document.querySelector('.icon-pm');
+                const maskIcon = document.querySelector('.recomendation img[alt="Uso de máscara"]');
+                const exerciseIcon = document.querySelector('.recomendation.ml-5 img[alt="Exercícios ao ar livre"]');
             
                 if (pm25Value <= 25) {
                     infoHeader.style.backgroundColor = '#a8e05f'; 
-                    smileIcon.src = 'img/teste img/carinhaBom.png'; 
+                    smileIcon.src = '../IMG/newCt/IconeVerde.png'; 
                     infoTitle.textContent = 'Bom';
-                    pmIcon.src = 'img/teste img/pmBom.png';
+                    pmIcon.src = '../IMG/newCt/mVerde.png'; 
+                    maskIcon.src = '../IMG/newCt/Bverde.png'; 
+                    exerciseIcon.src = '../IMG/newCt/Jverde.png'; 
                 } else if (pm25Value <= 50) {
-                    infoHeader.style.backgroundColor = '#fdd74b'; 
-                    smileIcon.src = 'img/teste img/carinhaModerado.png'; 
+                    infoHeader.style.backgroundColor = '#fdd64b'; 
+                    smileIcon.src = '../IMG/newCt/IconeAmarelo.png'; 
                     infoTitle.textContent = 'Moderado';
-                    pmIcon.src = 'img/teste img/pmModerado.png'; 
+                    pmIcon.src = '../IMG/newCt/mAmarelo.png'; 
+                    maskIcon.src = '../IMG/newCt/BAmerela.png'; 
+                    exerciseIcon.src = '../IMG/newCt/JAmarela.png'; 
                 } else if (pm25Value <= 75) {
-                    infoHeader.style.backgroundColor = '#fe9b57'; 
-                    smileIcon.src = 'img/teste img/carinhaRuim.png';
+                    infoHeader.style.backgroundColor = '#ff9b57'; 
+                    smileIcon.src = '../IMG/newCt/IconeLaranja.png'; 
                     infoTitle.textContent = 'Ruim';
-                    pmIcon.src = 'img/teste img/pmRuim.png'; 
+                    pmIcon.src = '../IMG/newCt/mLaranja.png'; 
+                    maskIcon.src = '../IMG/newCt/MascLaranja.png'; 
+                    exerciseIcon.src = '../IMG/newCt//BLaranja.png'; 
                 } else if (pm25Value <= 125) {
                     infoHeader.style.backgroundColor = '#fe6a69'; 
-                    smileIcon.src = 'img/teste img/carinhaMuitoRuim.png';
+                    smileIcon.src = '../IMG/newCt/IconeVermelho.png'; 
                     infoTitle.textContent = 'Muito Ruim';
-                    pmIcon.src = 'img/teste img/pmMuitoRuim.png'; 
+                    pmIcon.src = '../IMG/newCt/mVermelho.png'; 
+                    maskIcon.src = '../IMG/newCt/MascVermelha.png'; 
+                    exerciseIcon.src = '../IMG/newCt/Bvermelha.png';  
                 } else {
-                    infoHeader.style.backgroundColor = '#683793'; 
-                    smileIcon.src = '../IMG/newCt/Iconepessimo.png'; 
+                    infoHeader.style.backgroundColor = '#ca5ce0'; 
+                    smileIcon.src = '../IMG/newCt/IconeRoxo.png'; 
                     infoTitle.textContent = 'Péssimo';
-                    pmIcon.src = '../IMG/newCt/RectanglePessimo.png';
+                    pmIcon.src = '../IMG/newCt/mRoxo.png'; 
+                    maskIcon.src = '../IMG/newCt/MascRoxa.png'; 
+                    exerciseIcon.src = '../IMG/newCt/IconRoxo.png';
                 }
             } else {
                 pm25Display.textContent = 'N/A';
-            }
+            }            
             
         } catch (error) {
             console.error('Erro ao buscar dados:', error);
